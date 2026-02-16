0426a7c82de7155cc8ae63ca65a98c6d24a50bf5
penpitchanuntago <68010826@kmitl.ac.th>
2026-02-16 10:13:03 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -2,10 +2,10 @@ export class Person {private age?: number;
     firstName: string;
     lastName: string;
 
-    constructor(age: number,firstName:string,lastName=string) {
+    constructor(age: number,firstName:string,lastName:string) {
         this.age = age;
-        this.firstname=firstName;
-        this.lastname=lastName;
+        this.firstName=firstName;
+        this.lastName=lastName;
     }
     setAge(age:number){}
     getAge(age:number){return this.age}
