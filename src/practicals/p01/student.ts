07327b270bdc6a8ad1ecf7cc9dca66addb69530b
penpitchanuntago <68010826@kmitl.ac.th>
2026-02-16 09:22:26 +0700

    push test

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,1 +1,8 @@
+class Student{
+    firstname:string ;
+    lastname : string;
+    printname(message?:string){
+    console.log('My name is',this.firstname,this.lastname);
+  }
+}
 export class Student {}
--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,13 @@
+class Person {
+    private age?: number;
+    firstName: string;
+    lastName: string;
+
+    constructor(age: number) {
+        this.age = age;
+    }
+    getFullname(){
+
+    }
+}
 export class Person {}
